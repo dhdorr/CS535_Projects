58bd50be7d6d6a9309c2ca42c390cffd8fb0523d
Derek Dorr <44913332+dhdorr@users.noreply.github.com>
2023-09-22 22:40:49 -0700

    Update cityProblem.py

--- Projects/P2_CityProblem/cityProblem.py
+++ Projects/P2_CityProblem/cityProblem.py
@@ -12,53 +12,49 @@ FAILED_TO_FIND = -1
 def city_problem():
 
     # Test each city for possible best starting city
-    for x, i in enumerate(city_distances):
+    for city, i in enumerate(city_distances):
 
         # Check if current city has enough fuel to get us to the next city
-        if mpg * city_fuel[x] >= i:
-            print(f"Able to drive from {x} to {(x + 1) % len(city_distances)}")
+        if mpg * city_fuel[city] >= i:
+            print(f"Able to drive from {city} to {(city + 1) % len(city_distances)}")
 
             # Start driving from current city
-            preferred_city = drive_cities(x, 0, x)
+            preferred_city = drive_cities(city, 0, city)
 
             # If preferred city is found, return immediately
             if preferred_city:
-                return x
+                return city
 
         # Not enough gas to leave current city
         else:
-            print(f"not enough gas at {x}")
+            print(f"not enough gas at {city}")
 
     # In case of failure
     return FAILED_TO_FIND
 
 
 def drive_cities(current_city, gas_reserves, starting_city):
-
     print("Arrived at: ", current_city)
+
     # Fuel up upon reaching each city
     fuel_up = gas_reserves + (mpg * city_fuel[current_city])
 
     # If you have enough fuel to get to the next city, then drive
     if fuel_up >= city_distances[current_city]:
 
-        # Loop around if currently at the last city
-        if (current_city + 1) % len(city_distances) != starting_city:
+        # Determine how much gas will be used driving to next city
+        drive_cost = fuel_up - city_distances[current_city]
+        next_city = (current_city + 1) % len(city_distances)
 
-            # Determine how much gas will be used driving to next city
-            drive_cost = fuel_up - city_distances[current_city]
-            next_city = (current_city + 1) % len(city_distances)
-
-            # Drive to next city
-            return drive_cities(next_city, drive_cost, starting_city)
-
-        # Next city will make a full loop! End the search
-        else:
+        # If you made a full loop, end the search
+        if next_city == starting_city:
             return True
 
+        # Drive to next city
+        return drive_cities(next_city, drive_cost, starting_city)
+
     # Not enough gas to drive to next city
-    else:
-        return False
+    return False
 
 
 if __name__ == '__main__':
