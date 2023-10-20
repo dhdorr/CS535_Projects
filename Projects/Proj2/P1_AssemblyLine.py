6df5a502ef63aea70243c06485c788344b033914
Derek Dorr <dhdorr@gmail.com>
2023-10-19 23:30:23 -0700

    draft dynamic prog
    only works when stations = 3. need to find a way to make matrix building dynamic

--- Projects/Proj2/P1_AssemblyLine.py
+++ Projects/Proj2/P1_AssemblyLine.py
@@ -52,18 +52,64 @@ def take3(durations, stations):
     sol_list = []
     start = 0
     count = 0
+    end = 0
+    dict2 = {}
 
+    # while True:
+    #     for i in range(stations):
+    #         temp = durations[start:len(durations) - (stations - i - 1)]
+    #         start = len(durations) - (stations - i - 1)
+    #
+    #         sol_list.append(temp)
+    #     solutions_dict[count] = sol_list
+    #     count += 1
+    #     break
+    # print(solutions_dict)
+    count = 1
     while True:
-        for i in range(stations):
-            temp = durations[start:len(durations) - (stations - i - 1)]
-            start = len(durations) - (stations - i - 1)
+        if count > 2:
+            break
 
-            sol_list.append(temp)
-        solutions_dict[count] = sol_list
+        start = 0
+        matrix = []
+        for j in range(len(durations) - (stations - 1)):
+            sol_list.append(j + 1)
+
+        for a in sol_list:
+            for b in sol_list:
+                for c in sol_list:
+                    if a + b + c == len(durations):
+                        matrix.append([a,b,c])
+        print(matrix)
+        testDict = {}
+        for i, m in enumerate(matrix):
+            itr = 0
+            testList = []
+            for j, n in enumerate(m):
+                temp = durations[itr:itr + n]
+
+                testList.append(temp)
+
+                itr += n
+            testDict[i] = testList
+        print(testDict)
+        testMatrix = []
+        for key in testDict:
+            should_discard = False
+            for k, val in enumerate(testDict[key]):
+                print(val)
+                for i in range(len(val)):
+                    if i < len(val)-1:
+                        if val[i] < val[i + 1]:
+                            print("discard")
+                            should_discard = True
+            if not should_discard:
+                testMatrix.append(testDict[key])
+
+        print(testMatrix)
+
         count += 1
         break
-    print(solutions_dict)
-
 
 
 if __name__ == '__main__':
