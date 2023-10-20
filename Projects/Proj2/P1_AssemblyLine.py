b944c3d29d15157629dab003acaa911c55b7c889
Derek Dorr <dhdorr@gmail.com>
2023-10-20 00:00:49 -0700

    Update P1_AssemblyLine.py

--- Projects/Proj2/P1_AssemblyLine.py
+++ Projects/Proj2/P1_AssemblyLine.py
@@ -1,49 +1,3 @@
-import math
-
-
-def assembly_line(durations, stations):
-    maxNum = 0
-    cutElement = 0
-    for i in range(len(durations)):
-        if durations[i] > maxNum:
-            maxNum = durations[i]
-            cutElement = i
-    goal_duration = durations[cutElement]
-    cut_durations = durations[:cutElement]
-    stations -= 1
-
-    total = 0
-    for j in range(len(cut_durations)-1, -1, -1):
-        if total >= goal_duration:
-            return total
-        total += cut_durations[j]
-
-
-def take2(durations, stations):
-    print("hellow")
-    station_list = [0] * stations
-    start = 0
-    print(station_list)
-    for i in range(stations-1):
-        for j in range(start, len(durations)):
-            print(durations[j]," ? ", durations[j+1])
-            if durations[j] >= durations[j+1]:
-                station_list[i] += durations[j]
-                print("if: ", station_list)
-
-            else:
-                station_list[i-1] += durations[j]
-                station_list[i] += durations[j+1]
-                print("else: ", station_list)
-
-            start += 1
-            break
-    start += 1
-    if durations[start] < durations[start + 1]:
-        station_list[stations - 2] += durations[start]
-        station_list[stations - 1] += durations[start + 1]
-    # station_list[stations-1] = durations[len(durations)-1]
-    print(station_list)
 
 
 def take3(durations, stations):
@@ -111,6 +65,13 @@ def take3(durations, stations):
             final_temp_list.append(v)
 
     print(final_temp_list)
+    maxNo = 0
+    for f in final_temp_list:
+        for g in f:
+            helpMe = sum(g)
+            if helpMe > maxNo:
+                maxNo = helpMe
+    print(maxNo)
 
     # print(testMatrix)
 
@@ -120,8 +81,5 @@ def take3(durations, stations):
 if __name__ == '__main__':
     durations = [15,15,30,30,45]
     stations = 3
-    # ans = assembly_line(durations, stations)
-    # print("answer = ", ans)
 
-    # take2(durations, stations)
     take3(durations, stations)
