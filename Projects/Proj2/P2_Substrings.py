1ac492ba8e4c1e42c15e7093879b542d15129138
Derek Dorr <dhdorr@gmail.com>
2023-10-21 16:14:42 -0700

    Update P2_Substrings.py

--- Projects/Proj2/P2_Substrings.py
+++ Projects/Proj2/P2_Substrings.py
@@ -1,31 +1,61 @@
-def dfs_v4(sol_dict, idx, myPath=None):
-    if not myPath:
-        myPath = []
+def string_graph_dfs(sol_dict, idx, visited=None):
+    # Create a path list
+    if not visited:
+        visited = []
 
-    res = [idx]
+    # Initialize path
+    path = [idx]
+
+    # If dfs cannot go further, return the path + the current node
     if not sol_dict[idx]:
-        return myPath + res
+        return visited + path
 
-    if idx in myPath:
-        return myPath
+    # Do not revisit nodes
+    if idx in visited:
+        return visited
 
-    child_path = dfs_v4(sol_dict, sol_dict[idx][0], myPath)
-    res = [idx] + child_path
+    # Build path from current node recursively
+    child_path = string_graph_dfs(sol_dict, sol_dict[idx][0], visited)
+    path += child_path
 
-    return res
+    return path
 
 
 def build_solution_graph(Strings):
+    # Graph of connected strings
     temp_dict = {}
+    # keep the spelling of each word
+    spelling_dict = {}
+
+    # Loop through input list
     for w in Strings:
         temp_dict[w] = []
+        # Spell first word
+        spelling_dict[w] = [i for i in w]
+
+        # Find a string such that the w-string is a substring of the w2-string
         for w2 in Strings:
             if len(w2) == len(w) + 1:
+                # Spell second word
+                spelling_dict[w2] = [i for i in w2]
                 is_valid = True
-                for a in w:
-                    if not a in w2:
+
+                # Check if w2 contains all of w
+                for c in spelling_dict[w]:
+                    if c not in spelling_dict[w2]:
                         is_valid = False
                         break
+
+                if is_valid:
+                    # Check if there is only a 1-letter difference between the spelling of w and w2
+                    for i, c in enumerate(spelling_dict[w2]):
+                        if c not in spelling_dict[w]:
+                            temp = w[:i] + c + w[i:]
+                            if temp != w2:
+                                is_valid = False
+                                break
+
+                # If w is a valid substring of w2, add w2 to the list
                 if is_valid:
                     temp_dict[w].append(w2)
     return temp_dict
@@ -36,15 +66,19 @@ def longest_string_chain(Strings):
     maxLen = 1
     answer = []
 
+    # Build a graph where each string-node has a list of strings, from the input
+    # The current string must be a substring of the next string.
     sol_dict = build_solution_graph(Strings)
 
+    # Run a dfs on every node in the graph
     for key in sol_dict.keys():
-        idx = key
         if not sol_dict[key]:
             continue
 
-        path_dict[key] = dfs_v4(sol_dict, idx)
+        # Build a list of paths from every node
+        path_dict[key] = string_graph_dfs(sol_dict, key)
 
+    # Optimize for longest path
     for k in path_dict.keys():
         if len(path_dict[k]) > maxLen:
             maxLen = len(path_dict[k])
