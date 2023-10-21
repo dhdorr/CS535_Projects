1ad05ee3f14c8d266153e8402d24045ecb68fa3d
Derek Dorr <dhdorr@gmail.com>
2023-10-20 21:14:48 -0700

    Update P2_Substrings.py

--- Projects/Proj2/P2_Substrings.py
+++ Projects/Proj2/P2_Substrings.py
@@ -10,29 +10,9 @@ def longest_substring(Strings):
                 for t in s:
                     if t not in string_dict[a]:
                         substr_list.pop()
-                        print("discard: ", s)
+                        # print("discard: ", s)
                         break
-    print(substr_list)
-
-    for pair in substr_list:
-        testList = [pair[0]]
-        print(testList)
-        visited = []
-        visited.append(testList)
-        i = 0
-        j = 0
-        while i < len(substr_list):
-            # print(pair)
-            # print(testList)
-            # print("visited: ", visited)
-            if substr_list[i][0] == testList[len(testList) - 1] and substr_list[i][1] not in testList:
-                testList.append(substr_list[i][1])
-                visited.append(testList)
-                j += 1
-                # print(testList)
-                i = 0
-            else:
-                i += 1
+    # print(substr_list)
 
     string_graph = {}
     for s in Strings:
@@ -40,12 +20,11 @@ def longest_substring(Strings):
 
     for s in substr_list:
         string_graph[s[0]].append(s[1])
-    print(string_graph)
+    # print(string_graph)
 
     possible_substrings = []
     maxLen = 2
     for key in string_graph.keys():
-        count = 0
         if not string_graph[key]:
             continue
 
@@ -53,17 +32,16 @@ def longest_substring(Strings):
         if len(temp_list) > maxLen:
             maxLen = len(temp_list)
             possible_substrings.append(temp_list)
-        print(possible_substrings)
+        # print(possible_substrings)
     return possible_substrings.pop()
-
 
 
 def dfs_v2(string_graph, start, count, visited=None):
     if visited is None:
         visited = []
+
     visited.append(start)
-    # print("count: ", count)
-    # print("visited: ", visited)
+
     for next in string_graph[start]:
         dfs_v2(string_graph, next, count + 1, visited)
     return visited
@@ -74,29 +52,9 @@ def build_substring_dict(Strings):
     for s in Strings:
         temp_list = [c for c in s]
         string_dict[s] = temp_list
-    print(string_dict)
+    # print(string_dict)
 
     return string_dict
-
-def diff_check(A, B):
-
-    A_dict = {}
-
-    if len(A) + 1 == len(B):
-        print(A, " ", B)
-        for a in A:
-            if a in A_dict:
-                A_dict[a] += 1
-            else:
-                A_dict[a] = 1
-
-        for b in B:
-            if b in A_dict:
-                A_dict[b] -= 1
-        # print(A_dict)
-        if sum(A_dict.values()) == 1:
-            return True
-    return False
 
 
 if __name__ == '__main__':
