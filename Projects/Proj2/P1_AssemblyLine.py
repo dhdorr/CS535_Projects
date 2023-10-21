a4f2697332163168cf78cfca958784c97b654bfb
Derek Dorr <dhdorr@gmail.com>
2023-10-21 10:42:49 -0700

    updates

--- Projects/Proj2/P1_AssemblyLine.py
+++ Projects/Proj2/P1_AssemblyLine.py
@@ -15,68 +15,55 @@ def take3(durations, stations):
     # Build a matrix of all possible combinations of tasks per station
     # I.E. [[1],[1],[1]] ... [[3],[3],[3]] and everything in between
     for i in range(1, stations + 1):
-        res = [[i, a, b] for a in range(1, stations + 1) for b in sol_list]
-        for r in res:
-            # Only keep station/tasks configuration if the total number of station tasks = number of tasks available
-            # I.E. discard [[1],[1],[1]] & [[3],[3],[3]] because 1 + 1 + 1 = 3 < 5. And 3 + 3 + 3 = 9 > 5.
-            if sum(r) == len(durations):
-                # Each element in the matrix, i, represents the amount of tasks that station(i) has.
-                matrix.append(r)
+        matrix += [[i, a, b] for a in range(1, stations + 1) for b in sol_list if (i + a + b) == len(durations)]
+
     # Using the precomputed tasks per station matrix, create a new matrix using the tasks available
     # Tasks are always in the order given.
     # I.E. [[1][1][3]] --> [[15],[15],[30,30,45]]
     for i, m in enumerate(matrix):
         # Track start position for the tasks to assign each station.
         itr = 0
-        testList = []
+        temp_list = []
 
         for j, n in enumerate(m):
-            testList.append(durations[itr:itr + n])
-
+            temp_list.append(durations[itr:itr + n])
             # n represents the number of tasks, so jump the start position by the number of tasks assigned
             itr += n
-
         # Keep all possible configurations for task/station allocation
-        masterList.append(testList)
+        masterList.append(temp_list)
 
-    # Get rid of configurations that are invalid
-    my_list = prune_list(masterList)
+    master_dict = build_pruned_dict(masterList)
 
     maxNo = 0
-    totals_list = []
+    totals_dict = {}
 
     # Get the maximum flow value of each valid station/task configuration.
-    for f in my_list:
-        for g in f:
+    for f in master_dict.keys():
+        for g in master_dict[f]:
             helpMe = sum(g)
             if helpMe > maxNo:
                 maxNo = helpMe
-        totals_list.append(maxNo)
+        totals_dict[f] = maxNo
 
-    min_total = totals_list[0]
     min_pos = 0
 
-    # Of all maximal flows, get the minimum flow.
-    for i, t in enumerate(totals_list):
-        if t < min_total:
-            min_total = t
-            min_pos = i
+    a = min(totals_dict.values())
 
-    print("optimized assembly line: ", my_list[min_pos])
-    print("optimized max wait: ", min_total)
+    for k in totals_dict.keys():
+        if totals_dict[k] == a:
+            min_pos = k
 
+    return {"station_config": master_dict[min_pos], "longest_duration": a}
 
-# Given a list of all possible assembly line configurations
-# Prune out all bad configurations
-def prune_list(master_list):
-    build_list = []
+def build_pruned_dict(master_list):
+    build_dict = {}
 
     # Discard configuration tuple if:
     # 1) m[i][j] < m[i][j + 1] and sum(m[i][0:j+1]) < m[i][j+1])
     #       if next element in station(i) tasks > current element AND sum(0, current element) < next element
     # 2) sum(m[i]) < m[i + 1][0]
     #       sum of station tasks durations < the duration of the next station's first task.
-    for m in master_list:
+    for idx, m in enumerate(master_list):
         discard_me = False
         for i in range(len(m)):
             for j in range(len(m[i])):
@@ -87,12 +74,13 @@ def prune_list(master_list):
                     if sum(m[i]) < m[i + 1][0]:
                         discard_me = True
         if not discard_me:
-            build_list.append(m)
-    return build_list
+            build_dict[idx] = m
+    return build_dict
 
 
 if __name__ == '__main__':
     durations = [15,15,30,30,45]
     stations = 3
 
-    take3(durations, stations)
+    answer = take3(durations, stations)
+    print(answer)
--- Projects/Proj2/P2_Substrings.py
+++ Projects/Proj2/P2_Substrings.py
@@ -3,6 +3,8 @@
 def take2(Strings):
     path_dict = {}
     string_index_dict = {}
+    maxLen = 1
+    answer = []
 
     for i, s in enumerate(Strings):
         string_index_dict[s] = i
@@ -11,8 +13,7 @@ def take2(Strings):
         print(test)
         path_dict[j] = test
 
-    maxLen = 1
-    answer = []
+
     print(path_dict)
     for k in path_dict.keys():
         if len(path_dict[k]) > maxLen:
@@ -21,24 +22,25 @@ def take2(Strings):
     return answer
 
 
-def dfs_v3(idx, Strings, string_index_dict, chain_path_dict=None):
+def dfs_v3(s_idx, Strings, string_index_dict, chain_path_dict=None):
     if chain_path_dict is None:
         chain_path_dict = {}
 
-    if idx in chain_path_dict:
-        return chain_path_dict[idx]
+    if s_idx in chain_path_dict:
+        return chain_path_dict[s_idx]
 
-    res = [Strings[idx]]
-    
-    for k, s in enumerate(Strings[idx]):
-        child = Strings[idx][:k] + Strings[idx][k+1:]
+    res = [Strings[s_idx]]
+
+    for c_idx in range(len(Strings[s_idx])):
+        # Remove a character from the String at index k
+        child = Strings[s_idx][:c_idx] + Strings[s_idx][c_idx+1:]
         if child in string_index_dict:
             child_path = dfs_v3(string_index_dict[child], Strings, string_index_dict, chain_path_dict)
             # Update our result if the new path is longer
             if len(child_path) + 1 > len(res):
-                res = [Strings[idx]] + child_path
+                res = [Strings[s_idx]] + child_path
 
-    chain_path_dict[idx] = res
+    chain_path_dict[s_idx] = res
 
     return res
 
