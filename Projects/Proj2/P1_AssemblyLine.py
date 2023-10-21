db9c4962dd5ba446d66a63ad8816343fedfaf617
Derek Dorr <dhdorr@gmail.com>
2023-10-21 16:33:41 -0700

    Update P1_AssemblyLine.py

--- Projects/Proj2/P1_AssemblyLine.py
+++ Projects/Proj2/P1_AssemblyLine.py
@@ -4,7 +4,9 @@ def optimize_assembly_line(durations, stations):
     sol_list = []
     matrix = []
     masterList = []
+    max_durations_dict = {}
 
+    # Create a list of the number of tasks any given station can have
     sol_list = [j+1 for j in range(len(durations) - (stations - 1))]
 
     # Build a matrix of all possible combinations of tasks per station
@@ -12,42 +14,36 @@ def optimize_assembly_line(durations, stations):
     for i in range(1, stations + 1):
         matrix += [[i, a, b] for a in range(1, stations + 1) for b in sol_list if (i + a + b) == len(durations)]
 
-    # Using the precomputed tasks per station matrix, create a new matrix using the tasks available
+    # Using the precomputed tasks per station configuration matrix, apply the tasks available
     # I.E. [[1][1][3]] --> [[15],[15],[30,30,45]]
     for m in matrix:
         # Track start position for the tasks to assign each station.
         itr = 0
         temp_list = []
 
+        # For every station, apply the list of task durations
         for n in m:
             temp_list.append(durations[itr:itr + n])
             # n is the number of tasks at the ith station, so jump the start position by n
             itr += n
+
         # Keep all possible configurations for task/station allocation
         masterList.append(temp_list)
 
+    # Discard invalid configurations
     master_dict = build_pruned_dict(masterList)
 
-    maxNo = 0
-    totals_dict = {}
-
-    # Get the maximum flow value of each valid station/task configuration.
+    # Get the maximum duration value of each station configuration.
     for f in master_dict.keys():
-        for g in master_dict[f]:
-            helpMe = sum(g)
-            if helpMe > maxNo:
-                maxNo = helpMe
-        totals_dict[f] = maxNo
+        max_durations_dict[f] = max([sum(g) for g in master_dict[f]])
 
-    min_pos = 0
+    # Of all the longest configurations, optimize for minimum duration
+    min_duration = min(max_durations_dict.values())
 
-    a = min(totals_dict.values())
+    # Get the key for the minimum duration
+    pos = [k for k in max_durations_dict.keys() if max_durations_dict[k] == min_duration]
 
-    for k in totals_dict.keys():
-        if totals_dict[k] == a:
-            min_pos = k
-
-    return {"stations_config": master_dict[min_pos], "longest_duration": a}
+    return {"stations_config": master_dict[pos[0]], "longest_duration": min_duration}
 
 def build_pruned_dict(master_list):
     build_dict = {}
