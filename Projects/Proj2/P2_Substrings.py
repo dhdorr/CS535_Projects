4b861f35319af710eceb485426a4f954222f4016
Derek Dorr <dhdorr@gmail.com>
2023-10-21 18:25:56 -0700

    reverse order result

--- Projects/Proj2/P2_Substrings.py
+++ Projects/Proj2/P2_Substrings.py
@@ -5,7 +5,7 @@ def string_graph_dfs(sol_dict, idx, visited=None):
 
     # Initialize path
     path = [idx]
-
+    # print(path)
     # If dfs cannot go further, return the path + the current node
     if not sol_dict[idx]:
         return visited + path
@@ -15,8 +15,10 @@ def string_graph_dfs(sol_dict, idx, visited=None):
         return visited
 
     # Build path from current node recursively
-    child_path = string_graph_dfs(sol_dict, sol_dict[idx][0], visited)
-    path += child_path
+    for c in range(len(sol_dict[idx])):
+        child_path = string_graph_dfs(sol_dict, sol_dict[idx][c], visited)
+        if len(child_path) + 1 > len(path):
+            path = [idx] + child_path
 
     return path
 
@@ -35,29 +37,32 @@ def build_solution_graph(Strings):
 
         # Find a string such that the w-string is a substring of the w2-string
         for w2 in Strings:
-            if len(w2) == len(w) + 1:
+            if len(w2) == len(w) - 1:
                 # Spell second word
                 spelling_dict[w2] = [i for i in w2]
                 is_valid = True
 
                 # Check if w2 contains all of w
-                for c in spelling_dict[w]:
-                    if c not in spelling_dict[w2]:
+                for c in spelling_dict[w2]:
+                    if c not in spelling_dict[w]:
                         is_valid = False
+                        # print("w2: ", w2, " w: ", w)
                         break
 
                 if is_valid:
                     # Check if there is only a 1-letter difference between the spelling of w and w2
-                    for i, c in enumerate(spelling_dict[w2]):
-                        if c not in spelling_dict[w]:
-                            temp = w[:i] + c + w[i:]
-                            if temp != w2:
+                    for i, c in enumerate(spelling_dict[w]):
+                        if c not in spelling_dict[w2]:
+                            temp = w2[:i] + c + w2[i:]
+                            if temp != w:
                                 is_valid = False
+                                # print("w2: ", w2, " w: ", w)
                                 break
 
                 # If w is a valid substring of w2, add w2 to the list
                 if is_valid:
                     temp_dict[w].append(w2)
+
     return temp_dict
 
 
